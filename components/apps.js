73563f887cde02e824658938589c2dfc70a70afb
Daisy Pineda <amayranipineda@gmail.com>
2021-02-09 15:34:47 -0800

    made jumbo section responsive

---
+++ components/apps.js
@@ -0,0 +1,10 @@
+let projectImages = document.querySelectorAll(".projects__projImgs__img")
+
+
+var text = ""
+for (let i = 0; i < projectImages.length; i++) {
+  
+
+  // text += projectImages[i];
+
+}
